2d4200040056b77cd46d9063cdb03c3768177c2e
SUYOG PAWAR <pawarsuyog330@gmail.com>
2023-11-27 13:24:00 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -73,10 +73,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecom_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
